fb103d59ad81b970a8eae9831574072e2a944ac3
siva626203 <sivakumar.626203@gmail.com>
2024-04-24 20:29:03 +0530

    added

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const AxiosInstance=axios.create()
 AxiosInstance.interceptors.request.use((config)=>{
-    config.baseURL =process.env.NODE_ENV === "development"
+    config.baseURL =
+      process.env.NODE_ENV === "development"
         ? "http://localhost:4000/api"
-        : process.env.REACT_PRODUCTION_URL;
+        : process.env.REACT_APP_PRODUCTION_URL;
     config.headers['Authorization']=`Bearer ${localStorage.getItem("token")}`
     return config
 })
